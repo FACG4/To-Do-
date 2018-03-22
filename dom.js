edccee58b14f73d565bf78e50f00fe433d4e642b
ramyalshurafa <ramyshurafa@hotmail.com>
2018-03-22 14:17:14 +0200

    final editing

--- dom.js
+++ dom.js
@@ -1,93 +1,115 @@
 var logic = todoFunctions;
-// part 2 linking it all together
-// The function here is called an iife,
-// it keeps everything inside hidden from the rest of our application
-
-///our code
-// var description = document.getElementById('description').value;
-// var date = document.getElementById('date').value;
-
 
 (function() {
 
-var state=[];
+  var state = [];
 
-  // This is the dom node where we will keep our todo
   var container = document.getElementById('todo-container');
   var addTodoForm = document.getElementById('add-todo');
 
 
 
-    // { id: -3, description: 'first todo' },
-    // { id: -2, description: 'second todo' },
-    // { id: -1, description: 'third todo' },
-   // this is our initial todoList
+
+  if (localStorage.getItem("state")) {
+    state = JSON.parse(localStorage.getItem("state"))
+
+  }
 
 
-  if (localStorage.getItem("state")  === null) {
-      // Code for localStorage/sessionStorage.
-      state = [];
-  //     //4.1.2
-
-} else{
-  state = JSON.parse(localStorage.getItem("state"))
-
-}
-
-
-
-  // This function takes a todo, it returns the DOM node representing that todo
   var createTodoNode = function(todo) {
 
     var todoNode = document.createElement('li');
 
-    // addTodoForm.addEventListener('submit', function(event){
-    //   event.preventDefault();
-    //   // logic.addTodo(state, description);
-    //   //console.log(description)
-    //
-    // });
+    var descriptionSpan = document.createElement('input');
+    var dateSpan = document.createElement('span');
 
-    // add span holding description
-    var span = document.createElement('span');
-    var span2 = document.createElement('span');
+    descriptionSpan.className = ('spanDesc');
+    dateSpan.className = ('spanDat');
 
-     span.className = ('span1');
-     span2.className = ('span2');
+    descriptionSpan.value = todo.description
+    descriptionSpan.disabled = 'disabled'
+    descriptionSpan.style.border = 'none'
+    dateSpan.textContent = todo.date;
 
-    span.textContent = todo.description ;
-    span2.textContent =  todo.date;
-
-    if(todo.done){
-      span.style.textDecoration = 'line-through';
-      span2.style.textDecoration = 'line-through';
+    if (todo.done) {
+      descriptionSpan.style.textDecoration = 'line-through';
+      dateSpan.style.textDecoration = 'line-through';
 
     }
-    todoNode.appendChild(span);
-    todoNode.appendChild(span2);
+    todoNode.appendChild(descriptionSpan);
+    todoNode.appendChild(dateSpan);
 
 
 
 
-    // this adds the delete button
     var deleteButtonNode = document.createElement('button');
     deleteButtonNode.addEventListener('click', function(event) {
       var newState = logic.deleteTodo(state, todo.id);
       update(newState);
     });
-    deleteButtonNode.textContent = 'Delete'
-    deleteButtonNode.className = ('button1');
-    // deleteButtonNode.style.color= 'red'
+    deleteButtonNode.textContent = ''
+    deleteButtonNode.className = 'button1';
 
     todoNode.appendChild(deleteButtonNode);
 
-    // add markTodo button
+
+    var editButtonNode = document.createElement('button');
+
+    editButtonNode.addEventListener('click', function(event) {
+      event.preventDefault()
+
+      if (descriptionSpan.disabled == true) {
+        console.log(1);
+        descriptionSpan.disabled = !descriptionSpan.disabled
+
+
+      } else {
+        descriptionSpan.disabled = !descriptionSpan.disabled
+        var descript = descriptionSpan.value;
+        var dat = dateSpan.textContent;
+        var newState = logic.deleteTodo(state, todo.id);
+        update(newState)
+
+        newState = logic.addTodo(state, descript, dat)
+        newState = update(newState)
+        sortTodo(newState)
+
+
+      }
+
+
+
+
+
+
+    });
+    editButtonNode.textContent = ''
+    editButtonNode.className = 'button1';
+    todoNode.appendChild(editButtonNode);
+    var nodeedit = document.createElement("i");
+    nodeedit.className = ('fas fa-pencil-alt');
+    editButtonNode.appendChild(nodeedit);
+
+
+
+
+
+
 
     var markButtonNode = document.createElement('button');
-    markButtonNode.textContent = 'Mark'
-  markButtonNode.className = ('button2');
+    markButtonNode.textContent = ''
+    markButtonNode.className = 'button2';
+    var nodee = document.createElement("i");
+    nodee.className = ('fas fa-trash-alt');
+    deleteButtonNode.appendChild(nodee);
+    var nodeee = document.createElement("i");
+    nodeee.className = ('fas fa-check');
+    markButtonNode.appendChild(nodeee);
 
-    // markButtonNode.style.color= 'red'
+
+
+
+
     todoNode.appendChild(markButtonNode);
 
     markButtonNode.addEventListener('click', function(event) {
@@ -100,43 +122,34 @@ var state=[];
     return todoNode;
   };
 
-  // bind create todo form
   if (addTodoForm) {
     addTodoForm.addEventListener('submit', function(event) {
-      // https://developer.mozilla.org/en-US/docs/Web/Events/submit
-      // what does event.preventDefault do?
-      // what is inside event.target?
+
       event.preventDefault();
-      var description = document.getElementById('description').value; // event.target ....
+      var description = document.getElementById('description').value;
       var date = document.getElementById('date').value;
 
-      // hint: todoFunctions.addTodo
-      var newState = logic.addTodo(state, description, date); // ?? change this!
-      // console.log(newState)
+      var newState = logic.addTodo(state, description, date);
+      document.getElementById('description').value = '';
       update(newState);
     });
   }
 
-  // you should not need to change this function
   var update = function(newState) {
     state = newState;
     renderState(state);
   };
 
-  // you do not need to change this function
   var renderState = function(state) {
     var todoListNode = document.createElement('ul');
 
 
-    state.forEach(function(todo){
+    state.forEach(function(todo) {
       todoListNode.appendChild(createTodoNode(todo));
     });
 
-    //localStorage
     localStorage.setItem("state", JSON.stringify(state));
-    //localStorage
 
-    // you may want to add a class for css
     container.replaceChild(todoListNode, container.firstChild);
   };
 
